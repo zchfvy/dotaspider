7053700a5750da8e9c0e7552dde7ece73bb5e61b
Jason Hamilton-Smith <hs.jason@gmail.com>
2017-05-09 05:17:08 -0700

    Commit everything at once
    Because that's a great way to do stuff.
    Really though, this is a small project, "Initial commit"

---
+++ README.md
@@ -0,0 +1,16 @@
+Tool to get semi-reasonable dota2 bulk match data.
+
+
+Usage
+-----
+
+Add account id (32 bit int) and "0" pairs to players.txt like so
+
+> 1234567,0
+> 9876543,0
+
+These should be of account ID's you "trust"
+
+You will also need a folder called 'matches'.
+
+Configuration optiosn available in scrape.py
---
+++ scrape.py
@@ -0,0 +1,43 @@
+import requests
+from urllib import urlencode
+import json
+import os
+
+API_KEY="6951CC04045EEBA78C54D9855DD26EBB"
+
+HISTORY_URI="https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?"
+DETAILS_URI="https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?"
+
+ROOT = os.path.dirname(os.path.realpath(__file__))
+OUT_DIR = os.path.join(ROOT, 'matches')
+
+PLAYERSFILE = 'players.txt'
+MAX_DOS = 3
+
+players = []
+with open(PLAYERSFILE) as pf:
+    for line in pf:
+        players.append(line)
+
+acc_id, dos = players.pop(0).split(',')
+
+r = requests.get(HISTORY_URI + urlencode({'key': API_KEY, 'account_id':acc_id})).text
+history = json.loads(r)
+for m in history['result']['matches']:
+    if len(m['players']) == 10:
+        data = requests.get(DETAILS_URI + urlencode({'key': API_KEY, 'match_id': m['match_id']})).text
+        with open(os.path.join(OUT_DIR, str(m['match_id']) + '.json'), 'w') as f:
+            f.write(data)
+
+        next_dos = int(dos) + 1
+        if next_dos <= MAX_DOS:
+            jsondata = json.loads(data)
+            for pl in [p['account_id'] for p in jsondata['result']['players']]:
+                if pl not in players:
+                    players.append(','.join([str(pl), str(next_dos)]))
+
+players.append(','.join([acc_id, dos]))
+
+with open(PLAYERSFILE, 'w+') as pf:
+    for pl in players:
+        pf.write(pl + '\n')
